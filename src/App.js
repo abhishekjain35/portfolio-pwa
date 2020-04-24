cf4a74d771420a5e15b9994fe226480e912eb660
Abhishek Jain <abhishekjai35@gmail.com>
2020-04-24 13:20:42 +0530

    Added some packages, styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, lazy } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Home = lazy(() => import("./components/home/index"));
+const HomePage = () => (
+    <Suspense fallback={<h1>Loading...</h1>}>
+        <Home />
+    </Suspense>
+);
+
+const App = () => {
+    return <HomePage />;
+};
 
 export default App;
---
+++ src/components/home/style/HomeContainer.js
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+
+const HomeContainer = styled.div`
+    color: white;
+`;
+
+export default HomeContainer;
