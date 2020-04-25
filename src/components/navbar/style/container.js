98dba15f80d029dcf288a9449114028f2bbc7e4b
Abhishek Jain <abhishekjai35@gmail.com>
2020-04-25 19:18:40 +0530

    Removed cdn and added local svg with path

--- src/components/navbar/style/container.js
+++ src/components/navbar/style/container.js
@@ -1,5 +1,19 @@
-import  styled  from '@emotion/styled';
+import styled from "@emotion/styled";
 
 export const NavContainer = styled.div`
     background-color: #181818;
-`
+    position: absolute;
+    top: 0;
+    width: 60px;
+    height: 100%;
+    color: white;
+    display: flex;
+`;
+
+export const LogoDiv = styled.div`
+    background-color: #070707;
+    position: absolute;
+    padding: 8px 0;
+    width: 100%;
+    top: 0;
+`;
