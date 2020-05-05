1584a0626b6c7fd9167a65a1970cb77418cb4648
Abhishek Jain <abhishekjai35@gmail.com>
2020-05-05 20:18:11 +0530

    Added images to the animated box

--- src/components/about/style/style.js
+++ src/components/about/style/style.js
@@ -1,5 +1,9 @@
 import styled from "@emotion/styled";
 import { keyframes } from "@emotion/core";
+import ReactLogo from "../../../assets/ReactLogo.png";
+import MongoIcon from "../../../assets/MongoIcon.png";
+import NodeIcon from "../../../assets/NodeIcon.png";
+import ExpressIcon from "../../../assets/ExpressIcon.png";
 
 const spin = keyframes`
     0% {
@@ -39,47 +43,47 @@ export const Section = styled.section`
         border-width: 200px 0 200px 346px;
         transform-origin: 0 0;
     }
-    // & > div:first-child:after {
-    //     position: absolute;
-    //     content: url(//jacekjeznach.com/wp-content/themes/jj/css/../img/logo1.png);
-    // }
-    // & > div:after {
-    //     position: absolute;
-    //     color: #fff;
-    //     left: -311px;
-    //     top: -69px;
-    //     text-align: center;
-    //     opacity: 0.6;
-    // }
+    & > div:first-child:after {
+        position: absolute;
+        content: url(${ReactLogo});
+    }
+    & > div:after {
+        position: absolute;
+        color: #fff;
+        left: -311px;
+        top: -69px;
+        text-align: center;
+        opacity: 0.6;
+    }
     & > div:nth-child(2) {
         border-color: transparent transparent transparent rgba(50, 50, 50, 0.6);
         transform: rotateY(90deg) rotateZ(60deg) rotateX(180deg)
             translateY(-400px);
     }
-    // & > div:nth-child(2):after {
-    //     position: absolute;
-    //     content: url(//jacekjeznach.com/wp-content/themes/jj/css/../img/logo2.png);
-    // }
+    & > div:nth-child(2):after {
+        position: absolute;
+        content: url(${MongoIcon});
+    }
     > div:nth-child(3) {
         border-color: transparent transparent transparent rgba(50, 50, 50, 0.9);
         transform: rotateX(60deg) rotateY(19.5deg);
     }
-    // & > div:nth-child(3):after {
-    //     position: absolute;
-    //     content: url(//jacekjeznach.com/wp-content/themes/jj/css/../img/logo3.png);
-    // }
+    & > div:nth-child(3):after {
+        position: absolute;
+        content: url(${NodeIcon});
+    }
     & > div:nth-child(4) {
         border-color: transparent transparent transparent rgba(50, 50, 50, 0.8);
         transform: rotateX(-60deg) rotateY(19.5deg) translateX(-116px)
             translateY(-200px) translateZ(326px);
     }
-    // & > div:nth-child(4):after {
-    //     position: absolute;
-    //     // width: 150px;
-    //     content: url(//jacekjeznach.com/wp-content/themes/jj/css/../img/logo4.png);
-    //     // left: -315px;
-    //     // top: -91px;
-    // }
+    & > div:nth-child(4):after {
+        position: absolute;
+        // width: 150px;
+        content: url(${ExpressIcon});
+        // left: -315px;
+        // top: -91px;
+    }
 `;
 
 export const MainContainer = styled.div`
@@ -88,7 +92,7 @@ export const MainContainer = styled.div`
     width: 32%;
     height: 50%;
     position: absolute;
-    @media (max-width: 1255px){
+    @media (max-width: 1255px) {
         top: 14%;
     }
 `;
