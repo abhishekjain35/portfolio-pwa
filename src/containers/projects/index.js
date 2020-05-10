6d5b69b4f70d78ae17a383dffb6fbf8fbfe32c47
Abhishek Jain <abhishekjai35@gmail.com>
2020-05-10 14:19:13 +0530

    Added info for the first project

--- src/containers/projects/index.js
+++ src/containers/projects/index.js
@@ -2,8 +2,45 @@ import React from "react";
 import ProjectsPage from "./../../components/projects/index";
 
 const Projects = () => {
-
-    const data = 
+    const data = [
+        {
+            name: "Tshirt-store",
+            description:
+                "Made with MERN Stack, this is complete e-commerce t-shirt store with user and admin panels, stripe and braintree payment gateways.",
+            link: "https://github.com/abhishekjain35/Tshirt-store",
+            date: "March - Apr, 2019",
+        },
+        {
+            name: "",
+            description: "",
+            link: "",
+            date: "",
+        },
+        {
+            name: "",
+            description: "",
+            link: "",
+            date: "",
+        },
+        {
+            name: "",
+            description: "",
+            link: "",
+            date: "",
+        },
+        {
+            name: "",
+            description: "",
+            link: "",
+            date: "",
+        },
+        {
+            name: "",
+            description: "",
+            link: "",
+            date: "",
+        },
+    ];
 
     return <ProjectsPage />;
 };
