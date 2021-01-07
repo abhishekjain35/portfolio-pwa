60f5f1bb715663aaeeeb9b6ad5efdacc7f2f6f32
Abhishek Jain <abhishekjai35@gmail.com>
2021-01-07 22:32:05 +0530

    Added meta-og's and twitter cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Suspense, lazy } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
-import Fallback from "./components/reusable-components/fallback"
+import Fallback from "./components/reusable-components/fallback";
 import "./App.css";
 
 const Home = lazy(() => import("./pages/home"));
@@ -10,46 +10,48 @@ const Projects = lazy(() => import("./pages/projects"));
 const NotFound = lazy(() => import("./pages/not-found"));
 
 const HomePage = () => (
-    <Suspense fallback={<Fallback />}>
-        <Home />
-    </Suspense>
+  <Suspense fallback={<Fallback />}>
+    <Home />
+  </Suspense>
 );
 
 const AboutPage = () => (
-    <Suspense fallback={<Fallback />}>
-        <About />
-    </Suspense>
+  <Suspense fallback={<Fallback />}>
+    <About />
+  </Suspense>
 );
 
 const SkillsPage = () => (
-    <Suspense fallback={<Fallback />}>
-        <Skills />
-    </Suspense>
+  <Suspense fallback={<Fallback />}>
+    <Skills />
+  </Suspense>
 );
 
 const ProjectsPage = () => (
-    <Suspense fallback={<Fallback />}>
-        <Projects />
-    </Suspense>
+  <Suspense fallback={<Fallback />}>
+    <Projects />
+  </Suspense>
 );
 
 const NotFoundPage = () => (
-    <Suspense fallback={<Fallback />}>
-        <NotFound />
-    </Suspense>
+  <Suspense fallback={<Fallback />}>
+    <NotFound />
+  </Suspense>
 );
 
 const App = () => {
-    return (
-        <Switch>
-            <Route exact path="/about" component={AboutPage}></Route>
-            <Route exact path="/skills" component={SkillsPage}></Route>
-            <Route exact path="/projects" component={ProjectsPage}></Route>
-            <Route exact path="/" component={HomePage}></Route>
-            <Route path="/not-found" component={NotFoundPage} />
-            <Redirect to="/not-found" />
-        </Switch>
-    );
+  return (
+    <div class="App">
+      <Switch>
+        <Route exact path="/about" component={AboutPage}></Route>
+        <Route exact path="/skills" component={SkillsPage}></Route>
+        <Route exact path="/projects" component={ProjectsPage}></Route>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path="/not-found" component={NotFoundPage} />
+        <Redirect to="/not-found" />
+      </Switch>
+    </div>
+  );
 };
 
 export default App;
